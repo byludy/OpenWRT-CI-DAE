7622e8793ef33cd3ae12a01ff21f441392e0c499
byludy <56289598+byludy@users.noreply.github.com>
2026-04-24 17:57:19 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -82,6 +82,7 @@ UPDATE_PACKAGE "luci-app-lucky" "sirpdboy/luci-app-lucky" "main"
 
 # 我新加的
 echo "CONFIG_PACKAGE_luci-app-diskman=y" >> $GITHUB_WORKSPACE/Config/GENERAL.txt
+echo "CONFIG_PACKAGE_luci-app-filemanager=y" >> $GITHUB_WORKSPACE/Config/GENERAL.txt
 
 #UPDATE_PACKAGE "luci-app-netwizard" "sirpdboy/luci-app-netwizard" "main"
 #echo "CONFIG_PACKAGE_luci-app-netwizard=y" >> $GITHUB_WORKSPACE/Config/GENERAL.txt
